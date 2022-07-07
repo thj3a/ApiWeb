1f2c48a1568460297cea8f79344cc1fee2022637
Arthur Santâna <arthur.ea@hotmail.com>
2022-07-07 17:47:21 -0300

    implement basic way to show information on index

--- WebAppApi/Controllers/HomeController.cs
+++ WebAppApi/Controllers/HomeController.cs
@@ -4,21 +4,39 @@ using Newtonsoft.Json.Serialization;
 using System.Diagnostics;
 using WebAppApi.Models;
 using WebAppApi.Data;
+using System.Collections;
 
 namespace WebAppApi.Controllers
 {
   public class HomeController : Controller
   {
+    CityDB _cityDB = new();
+    
     private readonly ILogger<HomeController> _logger;
 
     public HomeController(ILogger<HomeController> logger)
     {
       _logger = logger;
     }
+    public IEnumerable Get()
+    {
+      return _cityDB.GetCities();
+    }
 
+    // GET api/employeeapi/5
+    public Cities Get(int id)
+    {
+      return _cityDB.GetCity(id);
+    }
     public IActionResult Index()
     {
-      return View(); 
+
+      //var json = System.Web.Http.GlobalConfiguration.Configuration.Formatters.JsonFormatter;
+      //json.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver();
+      //json.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+      var settings = new JsonSerializerSettings() { ContractResolver = new CamelCasePropertyNamesContractResolver(), ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore };
+      object model = JsonConvert.SerializeObject(Get(), Formatting.None, settings);
+      return View(model); 
     }
 
     public IActionResult Privacy()
--- WebAppApi/wwwroot/js/CitiesService.js
+++ WebAppApi/wwwroot/js/CitiesService.js
@@ -1,49 +1,49 @@
-﻿myApp.factory('citiesService', ['$http', '$q', function ($http, $q) {
-    var factory = {}
-    factory.get = function () {
-        var deferred = $q.defer();
-        $http.get("/Home")
-        //    .success(function (data, code, headers, config) { deferred.resolve(data); })
-        //    .error(function (msg, code, headers, config) { deferred.reject(msg); });
-        return deferred.promise;
-    }
+﻿//myApp.factory('citiesService', ['$http', '$q', function ($http, $q) {
+//    var factory = {}
+//    factory.get = function () {
+//        var deferred = $q.defer();
+//        $http.get("/Home")
+//        //    .success(function (data, code, headers, config) { deferred.resolve(data); })
+//        //    .error(function (msg, code, headers, config) { deferred.reject(msg); });
+//        return deferred.promise;
+//    }
 
-    factory.add = function (newCity) {
-        var deferred = $q.defer();
-        $http.post(newCity)
-        //    .success(function (data, code, headers, config) { deferred.resolve(data); })
-        //    .error(function (msg, code, headers, config) { deferred.reject(msg); });
-        return deferred.promise;
-    }
+//    factory.add = function (newCity) {
+//        var deferred = $q.defer();
+//        $http.post(newCity)
+//        //    .success(function (data, code, headers, config) { deferred.resolve(data); })
+//        //    .error(function (msg, code, headers, config) { deferred.reject(msg); });
+//        return deferred.promise;
+//    }
 
-    //To get details of a particular employee to be edited.
-    factory.edit = function (id) {
-        var deferred = $q.defer();
-        $http.get("Cities/" + id)
-        //    .success(function (data, code, headers, config) {
-        //    deferred.resolve(data);
-        //})
-        //    .error(function (msg, code, headers, config) { deferred.reject(msg); });
-        return deferred.promise;
-    }
+//    //To get details of a particular employee to be edited.
+//    factory.edit = function (id) {
+//        var deferred = $q.defer();
+//        $http.get("Cities/" + id)
+//        //    .success(function (data, code, headers, config) {
+//        //    deferred.resolve(data);
+//        //})
+//        //    .error(function (msg, code, headers, config) { deferred.reject(msg); });
+//        return deferred.promise;
+//    }
 
-    //To save changes to the edited employee
-    factory.update = function (city) {
-        var deferred = $q.defer();
-        $http.put("Cities/" + newCity.Id, city)
-        //    .success(function (data, code, headers, config) { deferred.resolve(data); })
-        //    .error(function (msg, code, headers, config) { deferred.reject(msg); });
-        return deferred.promise;
-    }
+//    //To save changes to the edited employee
+//    factory.update = function (city) {
+//        var deferred = $q.defer();
+//        $http.put("Cities/" + newCity.Id, city)
+//        //    .success(function (data, code, headers, config) { deferred.resolve(data); })
+//        //    .error(function (msg, code, headers, config) { deferred.reject(msg); });
+//        return deferred.promise;
+//    }
 
-    factory.delete = function (id) {
-        var deferred = $q.defer();
-        $http.delete("Cities/" + id)
-        //    .success(function (data, code, headers, config) {
-        //    deferred.resolve(data);
-        //})
-        //    .error(function (msg, code, headers, congig) { deferred.reject(msg); });
-        return deferred.promise;
-    }
-    return factory;
-}])
+//    factory.delete = function (id) {
+//        var deferred = $q.defer();
+//        $http.delete("Cities/" + id)
+//        //    .success(function (data, code, headers, config) {
+//        //    deferred.resolve(data);
+//        //})
+//        //    .error(function (msg, code, headers, congig) { deferred.reject(msg); });
+//        return deferred.promise;
+//    }
+//    return factory;
+//}])
